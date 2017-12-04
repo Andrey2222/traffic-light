150c88bd90d051a96ce4a0320534e255841c1162
John Doe <johndoe@example.com>
2017-12-04 23:21:57 +0200

    work trafik-light

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,17 @@
 document.getElementsByTagName("button")[0].addEventListener("click", function(){
-	var first = document.getElementsByClassName("stop")[0];
-	var second = document.getElementsByClassName("ready")[0];
-	var therd = document.getElementsByClassName("go")[0];
-	if (first == null){
-		this.classList.add("red")
+	var red = document.getElementsByClassName("red")[0];
+	var yellow = document.getElementsByClassName("yellow")[0];
+	var green = document.getElementsByClassName("green")[0];
+	var non = document.getElementsByClassName("non")[0];
+	if (non != null) {
+		non.classList.remove('non');
+  		document.getElementsByClassName('stop')[0].classList.add('red');
+	}else if (red != null){
+		red.classList.remove('red');
+  		document.getElementsByClassName('ready')[0].classList.add('yellow');
+	}else if (yellow != null){
+		yellow.classList.remove('yellow');
+  		document.getElementsByClassName('go')[0].classList.add('green');
 	}
  	
 });
